e4a67851090490dc582c1ca7dee6f1b735fa1f16
Abdulqudus <abdullateefqudusleeq@gmail.com>
2026-09-04 22:34:10 +0100

    Update tailwind.config.ts

--- my-app/tailwind.config.ts
+++ my-app/tailwind.config.ts
@@ -1,15 +1,13 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       fontFamily: {
         sans: ["var(--font-dm-sans)", "sans-serif"],
-      serif: ["var(--font-playfair)", "serif"],
+        serif: ["var(--font-playfair)", "serif"],
+      },
     },
   },
   plugins: [],
