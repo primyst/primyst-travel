07e44696d6e0d66b423f564857090515747df372
Abdulqudus <abdullateefqudusleeq@gmail.com>
2026-09-04 18:56:20 +0100

    Delete my-app/components directory

--- my-app/components/travel/TextLogo.tsx
+++
@@ -1,7 +0,0 @@
-export default function TextLogo({ large = false }: { large?: boolean }) {
-  return (
-    <div className={`font-black tracking-tight ${large ? "text-3xl" : "text-2xl"}`}>
-      <span className="text-[#0D9488]">Pure</span><span className="text-[#F97316]">Q</span>
-    </div>
-  );
-}
