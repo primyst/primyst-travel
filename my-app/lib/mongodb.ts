d7ac4b1760f618be7f96c29874c97daaffd540ad
Abdulqudus <abdullateefqudusleeq@gmail.com>
2026-09-05 11:50:44 +0100

    Update mongodb.ts

--- my-app/lib/mongodb.ts
+++ my-app/lib/mongodb.ts
@@ -1,25 +1,38 @@
-import mongoose from 'mongoose';
-
-const MONGODB_URI = process.env.MONGODB_URI;
-
-if (!MONGODB_URI) {
-  throw new Error('Please define MONGODB_URI in your environment variables.');
-}
+import mongoose from "mongoose";
 
 const globalForMongoose = global as typeof globalThis & {
-  mongoose?: { conn: typeof mongoose | null; promise: Promise<typeof mongoose> | null };
+  mongoose?: {
+    conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
+  };
 };
 
-const cached = globalForMongoose.mongoose || { conn: null, promise: null };
+const cached = globalForMongoose.mongoose || {
+  conn: null,
+  promise: null,
+};
 
 export async function connectToDatabase() {
-  if (cached.conn) return cached.conn;
+  const MONGODB_URI = process.env.MONGODB_URI;
+
+  if (!MONGODB_URI) {
+    throw new Error(
+      "Please define MONGODB_URI in your environment variables."
+    );
+  }
+
+  if (cached.conn) {
+    return cached.conn;
+  }
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, { bufferCommands: false });
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      bufferCommands: false,
+    });
   }
 
   cached.conn = await cached.promise;
   globalForMongoose.mongoose = cached;
+
   return cached.conn;
 }
